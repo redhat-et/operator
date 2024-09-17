65c4c565fb2a450fddd39221856ad4678bb06ca5
Tomas Turek <tturek@redhat.com>
2024-09-17 18:30:47 +0200

    PoC: Ensure to manage only predefined annotations and labes

--- internal/controller/common/action/base_action.go
+++ internal/controller/common/action/base_action.go
@@ -23,6 +23,8 @@ import (
 // OptimisticLockErrorMsg - ignore update error: https://github.com/kubernetes/kubernetes/issues/28149
 const OptimisticLockErrorMsg = "the object has been modified; please apply your changes to the latest version and try again"
 
+type EnsureOption func(current client.Object, expected client.Object) error
+
 type BaseAction struct {
 	Client   client.Client
 	Recorder record.EventRecorder
@@ -90,7 +92,7 @@ func (action *BaseAction) Requeue() *Result {
 	}
 }
 
-func (action *BaseAction) Ensure(ctx context.Context, obj client2.Object) (bool, error) {
+func (action *BaseAction) Ensure(ctx context.Context, obj client2.Object, opts ...EnsureOption) (bool, error) {
 	var (
 		expected client2.Object
 		ok       bool
@@ -98,9 +100,16 @@ func (action *BaseAction) Ensure(ctx context.Context, obj client2.Object) (bool,
 		result   controllerutil.OperationResult
 	)
 
+	if len(opts) == 0 {
+		opts = []EnsureOption{
+			EnsureSpec(),
+		}
+	}
+
 	if expected, ok = obj.DeepCopyObject().(client2.Object); !ok {
 		return false, errors.New("Can't create DeepCopy object")
 	}
+
 	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
 		result, err = controllerutil.CreateOrUpdate(ctx, action.Client, obj, func() error {
 			annoStr, find := obj.GetAnnotations()[annotations.PausedReconciliation]
@@ -111,24 +120,11 @@ func (action *BaseAction) Ensure(ctx context.Context, obj client2.Object) (bool,
 				}
 			}
 
-			currentSpec := reflect.ValueOf(obj).Elem().FieldByName("Spec")
-			expectedSpec := reflect.ValueOf(expected).Elem().FieldByName("Spec")
-			if currentSpec == reflect.ValueOf(nil) {
-				// object without spec
-				// return without update
-				return nil
-			}
-			if !expectedSpec.IsValid() || !currentSpec.IsValid() {
-				return errors.New("spec is not valid")
-			}
-			if !currentSpec.CanSet() {
-				return errors.New("can't set expected spec to current object")
-			}
-
-			// WORKAROUND: CreateOrUpdate uses DeepEqual to compare
-			// DeepEqual does not honor default values
-			if !equality.Semantic.DeepDerivative(expectedSpec.Interface(), currentSpec.Interface()) {
-				currentSpec.Set(expectedSpec)
+			for _, opt := range opts {
+				err = opt(obj, expected)
+				if err != nil {
+					return err
+				}
 			}
 
 			return nil
@@ -141,4 +137,85 @@ func (action *BaseAction) Ensure(ctx context.Context, obj client2.Object) (bool,
 	}
 
 	return result != controllerutil.OperationResultNone, nil
+}
+
+func EnsureSpec() EnsureOption {
+	return func(current client.Object, expected client.Object) error {
+		currentSpec := reflect.ValueOf(current).Elem().FieldByName("Spec")
+		expectedSpec := reflect.ValueOf(expected).Elem().FieldByName("Spec")
+		if currentSpec == reflect.ValueOf(nil) {
+			// object without spec
+			// return without update
+			return nil
+		}
+		if !expectedSpec.IsValid() || !currentSpec.IsValid() {
+			return errors.New("spec is not valid")
+		}
+		if !currentSpec.CanSet() {
+			return errors.New("can't set expected spec to current object")
+		}
+
+		// WORKAROUND: CreateOrUpdate uses DeepEqual to compare
+		// DeepEqual does not honor default values
+		if !equality.Semantic.DeepDerivative(expectedSpec.Interface(), currentSpec.Interface()) {
+			currentSpec.Set(expectedSpec)
+		}
+		return nil
+	}
+}
+
+func EnsureLabels(managedLabels ...string) EnsureOption {
+	return func(current client.Object, expected client.Object) error {
+		expectedLabels := expected.GetLabels()
+		if expectedLabels == nil {
+			expectedLabels = map[string]string{}
+		}
+		currentLabels := current.GetLabels()
+		if currentLabels == nil {
+			currentLabels = map[string]string{}
+		}
+		for _, anno := range managedLabels {
+			// delete annotation if it is not preset in expected object
+			if _, ok := expectedLabels[anno]; !ok {
+				delete(currentLabels, anno)
+				current.SetAnnotations(currentLabels)
+				continue
+			}
+			// set annotation if it is present
+			if val, ok := expectedLabels[anno]; ok {
+				currentLabels[anno] = val
+				continue
+			}
+		}
+		current.SetAnnotations(currentLabels)
+		return nil
+	}
+}
+
+func EnsureAnnotations(managedAnnotations ...string) EnsureOption {
+	return func(current client2.Object, expected client2.Object) error {
+		expectedAnno := expected.GetAnnotations()
+		if expectedAnno == nil {
+			expectedAnno = map[string]string{}
+		}
+		currentAnno := current.GetAnnotations()
+		if currentAnno == nil {
+			currentAnno = map[string]string{}
+		}
+		for _, anno := range managedAnnotations {
+			// delete annotation if it is not preset in expected object
+			if _, ok := expectedAnno[anno]; !ok {
+				delete(currentAnno, anno)
+				current.SetAnnotations(currentAnno)
+				continue
+			}
+			// set annotation if it is present
+			if val, ok := expectedAnno[anno]; ok {
+				currentAnno[anno] = val
+				continue
+			}
+		}
+		current.SetAnnotations(currentAnno)
+		return nil
+	}
 }
